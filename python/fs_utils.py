9fb4ca52a4842eae974a0e958ff1216eccc73ca1
d.karabitski <d.karabitski@dragonslake.com>
2023-08-03 17:13:17 +0200

    file_intersection added

--- python/fs_utils.py
+++ python/fs_utils.py
@@ -13,7 +13,17 @@ def print_dir(ifolder, ofile='assets.txt'):
 			for element in elements:
 				if element.is_dir():
 					dirs.append(element.path)
-				elif element.name.endswith(".uasset"):
+				elif element.name.endswith('.uasset'):
 					print(element.path)
 		sys.stdout = sys.original_stdout
 
+# Prints all common strings from if1 and if2 files into 'ofile'.
+def file_intersection(if1, if2, ofile = 'files.intersection'):
+	if1_lines = open(if1, 'r').readlines()
+	if2_lines = open(if2, 'r').readlines()
+	sys.original_stdout = sys.stdout
+	with open(ofile, 'w') as f:
+		sys.stdout = f
+		for line in [value for value in if1_lines if value in if2_lines]:
+			print(line)
+		sys.stdout = sys.original_stdout
